886077db1703ed6fcc1ce5f43af71dd855435a73
Khoiri Putra Mujiza <khoirimujiza@gmail.com>
2023-12-12 11:51:17 +0700

    first commit

---
+++ src/Dosen.java
@@ -0,0 +1,12 @@
+public class Dosen extends Elemen {
+    private Integer jumlahHariKerja;
+
+    public Dosen(String nama, Integer jumlahHariKerja){
+        super(nama);
+        this.jumlahHariKerja=jumlahHariKerja;
+    }
+
+    public Integer getJamSibuk(){
+        return jumlahHariKerja*8;
+    }
+}
